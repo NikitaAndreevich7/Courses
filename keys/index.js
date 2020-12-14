cd9a710047b6aafce304162a8697945f8b953cf0
Nikita Babanin <nik.babanin.97@gmail.com>
2020-12-14 17:54:46 +0300

    initial

---
+++ keys/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGODB_URI:`mongodb+srv://Nikita:vfktymrfz@cluster0.a38fm.mongodb.net/sohp`,
+  SESSION_SECRET: 'some sicret value',
+  EMAIL_FROM: 'nodejsse@gmail.com',
+  BASE_URL: 'http://localhost:3000'
+}
